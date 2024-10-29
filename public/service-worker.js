812729442950afad175e7ba12a60cd86f6bf49df
Ovidiu Mura <ovidiu.mura@yahoo.com>
2024-10-29 14:53:10 -0700

    adding stocks link

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.facceaaead7468624f496b43c137c3f7.js"
+  "/precache-manifest.d2e9fc3dce2d02cc89fafb624541673a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
