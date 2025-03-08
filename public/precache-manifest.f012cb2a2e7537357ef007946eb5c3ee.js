2717a3dbeca504f2eb71caa1f5fec64bfcd9afae
Ovidiu Mura <ovidiu.mura@yahoo.com>
2025-03-07 18:29:23 -0800

    adding stocks

---
+++ public/precache-manifest.f012cb2a2e7537357ef007946eb5c3ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea45d17e5bd6f9850f1065d1b596edca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27d6c9da598c81b70a94",
+    "url": "/static/css/2.4c8f9a96.chunk.css"
+  },
+  {
+    "revision": "a2bd97d17289446cf987",
+    "url": "/static/css/main.0b70f97e.chunk.css"
+  },
+  {
+    "revision": "27d6c9da598c81b70a94",
+    "url": "/static/js/2.a9da528f.chunk.js"
+  },
+  {
+    "revision": "0992c2dec64deec309bf3a8b58ec1167",
+    "url": "/static/js/2.a9da528f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2bd97d17289446cf987",
+    "url": "/static/js/main.34e57be2.chunk.js"
+  },
+  {
+    "revision": "213e9e7671eb593cd5a0",
+    "url": "/static/js/runtime-main.d57bbce4.js"
+  }
+]);
